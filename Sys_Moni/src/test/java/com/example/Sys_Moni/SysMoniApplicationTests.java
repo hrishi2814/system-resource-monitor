30b6b0dafafecb4bb2debed94fd0574ce2dfb7d4
hrishi2814 <thehrishikeshsonawane@gmail.com>
2024-10-21 11:35:05 +0530

    main branch?

---
+++ Sys_Moni/src/test/java/com/example/Sys_Moni/SysMoniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sys_Moni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysMoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
